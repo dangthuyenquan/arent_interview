a7c3a324761c8f671b48518736d654554826a711
dangthuyenquan <dangthuyenquan@gmail.com>
2023-07-22 15:04:41 +0700

    add gitignore + bebel menu header

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -3,63 +3,72 @@ import './style.css';
 
 export default class Header extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      openMenu: false
-    };
-    this.toggleMenuPopup = this.toggleMenuPopup.bind(this)
-  }
-
-  componentDidMount() {
-    document.body.onclick = () => {
-      this.setState({
-        openMenu: false
-      })
+    constructor(props) {
+        super(props);
+        this.state = {
+            openMenu: false
+        };
+        this.toggleMenuPopup = this.toggleMenuPopup.bind(this)
     }
-  }
 
-  toggleMenuPopup(e) {
-    e.stopPropagation();
-    this.setState({
-      openMenu: !this.state.openMenu
-    })
-  }
+    componentDidMount() {
+        document.body.onclick = () => {
+            this.setState({
+                openMenu: false
+            })
+        }
+    }
+
+    toggleMenuPopup(e) {
+        e.stopPropagation();
+        this.setState({
+            openMenu: !this.state.openMenu
+        })
+    }
 
 
 
 
-  render() {
+    render() {
 
-    return (
-      <div id="header">
-        <div className='container'>
-          <a href="#"><img className='logo' src="/img/logo.png" /></a>
-          <nav className='navi'>
-            <ul>
-              <li><a href="#/my-record"><img src="/img/icon-menu1.png" className='icon' />自分の記録</a></li>
-              <li><a href="#"><img src="/img/icon-menu2.png" className='icon' />チャレンジ</a></li>
-              <li><a href="#"><img src="/img/icon-menu3.png" className='icon' />お知らせ</a></li>
-            </ul>
-            <span className={'menu-toggle ' + (this.state.openMenu ? 'active' : '')} onClick={this.toggleMenuPopup}>
-              <span></span>
-            </span>
+        return (
+            <div id="header">
+                <div className='container'>
+                    <a href="#"><img className='logo' src="/img/logo.png" /></a>
+                    <nav className='navi'>
+                        <ul>
+                            <li><a href="#/my-record"><img src="/img/icon-menu1.png" className='icon' />自分の記録</a></li>
+                            <li><a href="#"><img src="/img/icon-menu2.png" className='icon' />チャレンジ</a></li>
+                            <li>
+                                <a href="#">
+                                    <img src="/img/icon-menu3.png" className='icon' />
+                                    <div className='babel'>
+                                        1
+                                    </div>
+                                    お知らせ
+                                </a>
 
-            {
-              this.state.openMenu ?
-                <div onClick={this.toggleMenuPopup} className='menu-popup'>
-                  <a href="#/my-record">自分の記録</a>
-                  <a href="#">体重グラフ</a>
-                  <a href="#">目標</a>
-                  <a href="#">選択中のコース</a>
-                  <a href="#/recommended">コラム一覧</a>
-                  <a href="#">設定</a>
+                            </li>
+                        </ul>
+                        <span className={'menu-toggle ' + (this.state.openMenu ? 'active' : '')} onClick={this.toggleMenuPopup}>
+                            <span></span>
+                        </span>
+
+                        {
+                            this.state.openMenu ?
+                                <div onClick={this.toggleMenuPopup} className='menu-popup'>
+                                    <a href="#/my-record">自分の記録</a>
+                                    <a href="#">体重グラフ</a>
+                                    <a href="#">目標</a>
+                                    <a href="#">選択中のコース</a>
+                                    <a href="#/recommended">コラム一覧</a>
+                                    <a href="#">設定</a>
+                                </div>
+                                : null
+                        }
+                    </nav>
                 </div>
-                : null
-            }
-          </nav>
-        </div>
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 }
--- src/section/Banner/index.js
+++ src/section/Banner/index.js
@@ -11,6 +11,16 @@ export default class Banner extends Component {
           <div className='circle_precent' role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
             <span className='date'>05/21</span>
             <span className='precent'>75%</span>
+            <svg style={{
+              position: 'absolute',
+              transform: 'rotate(280deg)',
+            }} viewBox="22 22 44 44">
+              <circle cx="44" cy="44" r="20.2" fill="none" strokeWidth="1" style={{
+                strokeDasharray: 126.92, strokeDashoffset: '31.73px',
+                stroke: 'currentcolor',
+                transition: 'stroke-dashoffset 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+              }}></circle>
+            </svg>
           </div>
         </div>
         <div className='banner_right'>
